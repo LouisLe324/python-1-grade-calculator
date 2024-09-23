1aa0eb52ece3e2574b7f4816c88426445b407329
Louis Le <louis.le@dawsoncollege.qc.ca>
2024-09-23 15:44:45 -0400

    put a summary here

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+labs = input("Enter the number of labs completed: ")
+quiz = input("Enter the number of quizzes completed: ")
+ass1 = input("Enter the grade received for Assignment 1: ")
+ass2 = input("Enter the grade received for Assignment 2: ")
+ass3 = input("Enter the grade received for Assignment 3: ")
+ass4 = input("Enter the grade received for Assignment 4: ")
+mid1 = input("Enter the grade received for Midterm 1: ")
+mid2 = input("Enter the grade received for Midterm 2: ")
+final = input("Enter the grade received for the Final Exam: ")
+midfinalprep = input("Enter the grade received for Midterm and Final Preparation: ")
+final_grade = 
+
