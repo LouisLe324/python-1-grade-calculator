67f09822039fec0b7dc3b5c5a748e4055d247e9c
Louis Le <louis.le@dawsoncollege.qc.ca>
2024-09-25 15:38:57 -0400

    complete assignement

--- assignment1.py
+++ assignment1.py
@@ -7,6 +7,18 @@ ass4 = input("Enter the grade received for Assignment 4: ")
 mid1 = input("Enter the grade received for Midterm 1: ")
 mid2 = input("Enter the grade received for Midterm 2: ")
 final = input("Enter the grade received for the Final Exam: ")
-midfinalprep = input("Enter the grade received for Midterm and Final Preparation: ")
-final_grade = 
+midfinalprep = input("Enter the grade received for the Midterm and Final Preparation: ")
+labsgr = ((float(labs)/6) * (20/100)) * 100
+quizgr = ((float(quiz)/6) * (15/100)) * 100
+ass1gr = float(ass1) * (4/100)
+ass2gr = float(ass2) * (4/100)
+ass3gr = float(ass3) * (4/100)
+ass4gr = float(ass4) * (4/100)
+mid1gr = float(mid1) * (12.5/100)
+mid2gr = float(mid2) * (12.5/100)
+finalgr = float(final) * (18/100)
+midfinalprepgr = float(midfinalprep) * (6/100)
+finalgrade = labsgr + quizgr + ass1gr + ass2gr + ass3gr + ass4gr + mid1gr + mid2gr + finalgr + midfinalprepgr
+finalgraderound = round(finalgrade)
+print("Your grade is " + str(finalgraderound))
 
